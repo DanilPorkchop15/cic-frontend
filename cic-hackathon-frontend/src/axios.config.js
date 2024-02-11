5d99454537925f0ec75023d5accabe1223173085
danilPorkchop15 <danilkostin150705@gmail.com>
2024-02-11 16:42:07 +0300

    epic commit

--- cic-hackathon-frontend/src/axios.config.js
+++ cic-hackathon-frontend/src/axios.config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = 'https://01c8-185-237-225-20.ngrok-free.app';
+axios.defaults.baseURL = 'https://23cc-185-237-225-20.ngrok-free.app';
 axios.defaults.headers.common["ngrok-skip-browser-warning"] = "69420";
 axios.defaults.headers.common["Content-Type"] = "application/json";
