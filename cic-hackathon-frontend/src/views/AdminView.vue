2969d76bd3786bdcf9fee93f68458a2146c69cc4
danilPorkchop15 <danilkostin150705@gmail.com>
2024-02-11 08:45:02 +0300

    cool commit

--- cic-hackathon-frontend/src/views/AdminView.vue
+++ cic-hackathon-frontend/src/views/AdminView.vue
@@ -1,6 +1,6 @@
 <script setup >
   import axios from "axios";
-  import {onBeforeMount, onMounted, ref} from "vue";
+  import {onBeforeMount, onMounted, ref, watch} from "vue";
   import {useUserStore} from "@/stores/user.ts";
   import {useRouter} from "vue-router";
 
@@ -9,6 +9,17 @@
   const getMarkers = async () => {
     try {
       const response = await axios.get("/api/markers");
+
+      // Process the response data
+      return response.data
+    } catch (error) {
+      // Handle the error
+      console.error("Error fetching data:", error.message);
+    }
+  }
+  const getCategory = async () => {
+    try {
+      const response = await axios.get("/api/status");
 
       // Process the response data
       return response.data
@@ -31,11 +42,38 @@
       console.error("Error deleting data:", error.message);
     }
   }
+  const deleteCategory = async (categoryId, index) => {
+    try{
+      await axios.delete(`/api/status/?id=${categoryId}`, {
+        headers: {
+          "Authorization": `Bearer ${sessionStorage.getItem("jwt")}`
+        }
+      })
+      categoryData.value.splice(index, 1)
+    } catch (error) {
+      // Handle the error
+      console.error("Error deleting data:", error.message);
+    }
+  }
+  const categoryData = ref([])
+  const categoryName = ref("")
 
+  const addCategory = async () => {
+    try{
+      await axios.post(`/api/status`)
+      markersData.value.splice(index, 1)
+    } catch (error) {
+      // Handle the error
+      console.error("Error deleting data:", error.message);
+    }
+  }
   const markersData = ref([])
   onMounted(()=>{
     getMarkers().then((data)=>{
       markersData.value = data
+    })
+    getCategory().then((data) => {
+      categoryData.value = data
     })
   })
   onBeforeMount(() => {
@@ -43,6 +81,10 @@
       router.push('/login')
     }
   })
+
+watch(markersData, ()=>{
+  getMarkers()
+})
 </script>
 
 <template>
@@ -61,6 +103,10 @@
             <figure class="item__figure" v-if="marker.image">
               <img :src="marker.image" :alt="marker.description" class="item__img">
             </figure>
+            <label><input type="checkbox" name="isRepair" id="ieRepair" @change="updateMarker"
+                          v-model="marker.isRepair"> Ремонт идет</label>
+            <label><input type="checkbox" name="isValidate" id="isValidate" @change="updateMarker"
+                          v-model="marker.isValidate"> Валидно</label>
             <button class="item__delete" @click="deleteMarkers(marker.id, index)">✕</button>
           </div>
         </div>
@@ -69,14 +115,14 @@
     <main class="admin-page">
       <h3 class="admin-page__title">Категории</h3>
       <div class="categories-list">
-        <div class="categories-list__item" v-for="(category, index) in markersData" :key="category.id">
-          <div class="category-item">
-
-            <button class="item__delete" @click="deleteMarkers(category.id, index)">✕</button>
+        <div class="categories-list__item" v-for="(category, index) in categoryData" :key="category.id" v-if="categoryData">
+          <div class="category-item" >
+            <p>Название категории: {{category.name}}</p>
+            <button class="item__delete" @click="deleteCategory(category.id, index)">✕</button>
           </div>
         </div>
         <form action="" class="add-category" @submit.prevent="addCategory">
-          <input type="text" name="newCategory" id="newCategory" class="add-category__name" placeholder="Введите название категории"/>
+          <input type="text" name="newCategory" id="newCategory" class="add-category__name" placeholder="Введите название категории" v-model="categoryName" required/>
           <input type="submit" value="+" class="add-category__button"/>
         </form>
       </div>
